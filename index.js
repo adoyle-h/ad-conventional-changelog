110b12e190fcb996d5ad221769ff83ff9b78c2d5
ADoyle <adoyle.h@gmail.com>
2016-04-06 16:31:51 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var wrap = require('word-wrap');
+var Path = require('path');
+var packageJSON = require(Path.resolve(process.cwd(), 'package.json'));
 
 // This can be any kind of SystemJS compatible module.
 // We use Commonjs here, but ES6 or AMD would do just
@@ -20,9 +22,14 @@ module.exports = {
     // template and will keep empty lines.
     prompter: function(cz, commit) {
         var maxLineWidth = 100;
-        var advice = '\n第一行建议在 ' + maxLineWidth + ' 字以内。其他行若超过 ' + maxLineWidth + ' 字会自动换行。\n';
+        var advice = '【提示】第一行建议在 ' + maxLineWidth + ' 字以内。其他行若超过 ' + maxLineWidth + ' 字会自动换行。\n';
 
         console.log(advice);
+
+        var promptType = packageJSON.config.commitizen.promptType;
+        if (promptType !== 'list') {
+            promptType = 'rawlist';
+        }
 
         // Let's ask some questions of the user
         // so that we can populate our commit
@@ -32,38 +39,47 @@ module.exports = {
         // You can also opt to use another input
         // collection library if you prefer.
         cz.prompt([{
-            type: 'list',
+            type: promptType,
             name: 'type',
             message: '选择类型',
+            default: '1',
             choices: [{
+                key: '1',
                 name: 'feat:     新的功能',
                 value: 'feat',
             }, {
-                name: 'fix:     修复 BUG',
+                key: '2',
+                name: 'fix:      修复 BUG',
                 value: 'fix',
             }, {
+                key: '3',
                 name: 'docs:     更改文档',
                 value: 'docs',
             }, {
-                name: 'style:    代码风格调整（如空白符、格式、少分号等等）',
+                key: '4',
+                name: 'style:    代码风格调整（如调整缩进、格式、补上分号等等）',
                 value: 'style',
             }, {
-                name: 'refactor: 重构代码（即不修复错误，也不添加新功能，不影响功能）',
+                key: '5',
+                name: 'refactor: 重构代码（不修复错误，不添加新功能，不影响功能）',
                 value: 'refactor',
             }, {
-                name: 'perf:     提升性能的代码修改',
+                key: '6',
+                name: 'perf:     提升性能的修改',
                 value: 'perf',
             }, {
+                key: '7',
                 name: 'test:     测试相关的修改',
                 value: 'test',
             }, {
+                key: '8',
                 name: 'chore:    构建工具的修改',
                 value: 'chore',
             }],
         }, {
             type: 'input',
             name: 'scope',
-            message: '编码修改的范围 (scope):\n',
+            message: '改动范围 (scope)【回车跳过】:\n',
         }, {
             type: 'input',
             name: 'subject',
@@ -71,15 +87,15 @@ module.exports = {
         }, {
             type: 'input',
             name: 'body',
-            message: '详细描述这次的修改内容:\n',
+            message: '详细描述这次的修改内容【回车跳过】:\n',
         }, {
             type: 'input',
             name: 'breaking',
-            message: 'Breaking Changes:\n',
+            message: 'Breaking Changes【回车跳过】:\n',
         }, {
             type: 'input',
             name: 'footer',
-            message: '相关任务或者 Issue（以 # 开头会自动 Close Issue）:\n',
+            message: '相关任务或者 Issue（以 # 开头会自动 Close Issue）【回车跳过】:\n',
         }], function(answers) {
             var wrapOptions = {
                 trim: true,
@@ -112,7 +128,7 @@ module.exports = {
                 if (footer.startsWith('#')) {
                     msg += '\n\nCloses ' + footer;
                 } else {
-                    msg += '\n\n' + footer;
+                    msg += '\n\nReference: ' + footer;
                 }
             }
             commit(msg);
